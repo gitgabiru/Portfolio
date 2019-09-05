175b4d895ead5d64d6263d3dbaa3636d9fa0676e
Gabriel <gabrielsoares.prog@gmail.com>
2019-09-05 10:44:20 -0300

    add contest 1186

---
+++ competition algorithm/codeforces/contest_1186/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    if (m < n || k < n)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+    return 0;
+}
