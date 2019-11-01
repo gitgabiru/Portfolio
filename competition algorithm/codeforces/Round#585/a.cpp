c8b3b93da3da061076c2f6d636e77ad458b70ec8
Gabriel <gabrielsoares.prog@gmail.com>
2019-11-01 11:33:58 -0300

    ++

---
+++ competition algorithm/codeforces/Round#585/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a1, a2, k1, k2, n;
+    int min = 0, max = 0, aux_min, aux_max;
+
+    cin >> a1 >> a2 >> k1 >> k2 >> n;
+
+    aux_min = (a1 * k1) + (a2 * k2);
+
+    cout << min << endl;
+
+    return 0;
+}
